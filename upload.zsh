d03a2173a0bf81434b94e01cb2b48fd96b213191
hg42@gmx.net <hg42@gmx.net>
2014-06-09 23:57:50 +0200

    allow two different mount points
    --HG--
    branch : hg42

--- upload.zsh
+++ upload.zsh
@@ -1,7 +1,29 @@
 
 source config.zsh
 
-#pumount $smoothie_disk_device
-pmount $smoothie_disk_device
-cp -v $firmware_built $firmware_file
-pumount $smoothie_disk_device
+if [[ -d $smoothie_upload_dir2 ]]; then
+  cp -v $firmware_built $smoothie_upload_dir2/$firmware_file
+  if [[ -e $smoothie_upload_dir2/$firmware_file ]]; then
+    echo ok
+  else
+    echo FAILED
+  fi
+else
+  if [[ -d $smoothie_upload_dir ]]; then
+    cp -v $firmware_built $smoothie_upload_dir/$firmware_file
+    if [[ -e $smoothie_upload_dir/$firmware_file ]]; then
+      echo ok
+    else
+      echo FAILED
+    fi
+  else
+    pmount $smoothie_disk_device                                &&
+      cp -v $firmware_built $smoothie_upload_dir/$firmware_file
+    pumount $smoothie_disk_device
+    if [[ -e $smoothie_upload_dir/$firmware_file ]]; then
+      echo ok
+    else
+      echo FAILED
+    fi
+  fi
+fi
