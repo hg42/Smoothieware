1ad28658aa020b5416887b6274d26f1beb7e93dd
hg42@gmx.net <hg42@gmx.net>
2014-06-24 21:14:40 +0200

    move excluded_modules from config.zsh to build.zsh

--- build.zsh
+++ build.zsh
@@ -3,17 +3,22 @@
 
 source config.zsh
 
-if [[ -n $excluded_modules ]]; then
-  echo "-- exclude modules: $excluded_modules"
-  export EXCLUDED_MODULES=$excluded_modules
-fi
+excluded_modules="tools/touchprobe tools/zprobe tools/laser utils/panel"
+#excluded_modules="$excluded_modules utils/player"
+#excluded_modules="$excluded_modules tools/temperaturecontrol"
+#excluded_modules="$excluded_modules tools/switch"
 
 make_options=()
 
-#export GPFLAGS=-fpermissive
-
 #export BUILD_TYPE=Debug
-#export NONETWORK=1
+export NONETWORK=1
+
+if [[ -n $excluded_modules ]]; then
+  echo "-- exclude modules: $excluded_modules"
+  #export EXCLUDED_MODULES=$excluded_modules
+  make_options=(EXCLUDED_MODULES="$excluded_modules")
+fi
+
 
 if make $make_options all; then
   if $upload_after_build; then
